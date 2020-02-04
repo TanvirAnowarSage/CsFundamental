cd0f58ea9d3ea9e802b469ea77faa294a7fdaeb3
Tanvir Anowar <tanvir.anowar@sage.com>
2020-02-04 12:46:58 +0000

    Initial Commit

---
+++ CsFundamental/CsFundamental/Threading.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace CsFundamental
+{
+    public class Threading
+    {       
+
+        public Threading()
+        {
+            Thread thread = new Thread(new ThreadStart(ThreadMethod));
+
+            thread.Start();
+
+        }
+
+        private void ThreadMethod()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+                Thread.Sleep(0);
+            }
+        }
+        
+    }
+}
